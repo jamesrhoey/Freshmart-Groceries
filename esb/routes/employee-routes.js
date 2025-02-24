e82314d70210bc1866568dfad54bcd692d78e52f
James Rhoey De Castro <116045031+jamesrhoey@users.noreply.github.com>
2025-02-24 16:03:42 +0800

    Update employee-routes.js

--- esb/routes/employee-routes.js
+++ esb/routes/employee-routes.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
-// Correctly import employee controller functions
+
 const { addEmployee, fetchEmployees, deleteEmployee } = require('../controllers/employee-controller');
 
-// Define routes
+
 router.post('/new', addEmployee);
 router.get('/all', fetchEmployees);
 router.delete('/:id', deleteEmployee);
